3aa984a5f4f8e519c639a0ef0a735bb05092b0fe
Joseph Mearman <joseph@mearman.co.uk>
2024-10-09 05:59:41 +0100

    style: update .editorconfig for consistent indentation and clean up .gitignore; remove unnecessary whitespace in source files

--- src/api/isThemeMode.ts
+++ src/api/isThemeMode.ts
@@ -1,6 +1,5 @@
 import { ThemeMode } from "../types";
 
-
 export function isThemeMode(
 	mode: string | undefined | null
 ): mode is ThemeMode {
--- src/context/useAppContext.tsx
+++ src/context/useAppContext.tsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { AppContextType } from "../types";
 import { AppContext } from "./AppContext";
 
-
 export function useAppContext(): AppContextType {
 	const context = useContext(AppContext);
 	if (context === undefined) {
